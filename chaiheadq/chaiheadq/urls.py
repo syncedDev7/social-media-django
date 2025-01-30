8791fdacfbe112217f164a45f762ecd035b6b8f5
Your Name <you@example.com>
2025-01-30 19:48:21 +0530

    Created protected routes and registratiojn and signup functionality

--- chaiheadq/chaiheadq/urls.py
+++ chaiheadq/chaiheadq/urls.py
@@ -19,7 +19,12 @@ from django.urls import path,include
 from django.conf.urls.static import settings
 from django.conf.urls.static import static 
 
+from django.contrib.auth.urls import views as auth_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tweet/',include('tweet.urls'))
+    path('tweet/',include('tweet.urls')),
+    path('accounts/',include('django.contrib.auth.urls'))
 ] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+
+
